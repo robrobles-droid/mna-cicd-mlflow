81f7a88b1ef09a1aaaf816b7278843cfa851b191
robrobles65 <rob.robles@gmail.com>
2026-09-17 20:20:43 -0600

    fix: remove unsupported skops_trusted_types param

--- src/train.py
+++ src/train.py
@@ -27,5 +27,4 @@ with mlflow.start_run():
 
    #Log metrics and model
    mlflow.log_metric("accuracy", accuracy)
-   mlflow.sklearn.log_model(model, artifact_path="model",
-   skops_trusted_types=["sklearn.tree._tree.Tree"])
+   mlflow.sklearn.log_model(model, artifact_path="model")
